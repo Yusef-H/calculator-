810b0a48aca06f67361d3f19b44a3e640cb0c76f
Yusef-H <yusefhalabi456@gmail.com>
2022-08-31 01:54:46 +0300

    display number buttons

--- calculator.js
+++ calculator.js
@@ -16,17 +16,91 @@ function divide(a, b){
 }
 
 function operate(operator, firstNum, secondNum){
+    firstNum = +firstNum;
+    secondNum = +secondNum;
+    var result;
     switch(operator){
         case '+':
-            add(firstNum, secondNum);
+            result = add(firstNum, secondNum);
             break;
         case '-':
-            subtract(firstNum, secondNum);
+            result = subtract(firstNum, secondNum);
             break;
         case '*':
-            multiply(firstNum, secondNum);
+            result = multiply(firstNum, secondNum);
             break;
         case '/':
-            divide(firstNum, secondNum);
+            result = divide(firstNum, secondNum);
+            break;
     }
+    return result;
 }
+
+function updateDisplayNumber(value){
+    const displayBoard = document.querySelector('.display');
+    if(displayBoard.textContent.length > 9){
+        displayBoard.style.fontSize = '40px';
+        displayBoard.textContent = 'MAX NUMBER LENGTH EXCEEDED';
+        return;
+    }
+    displayBoard.textContent = displayBoard.textContent + value;
+}
+
+function updateDisplay(value){
+    const displayBoard = document.querySelector('.display');
+    displayBoard.textContent = value.toString();
+}
+
+
+function handleNumberButtons(){
+    const numbersNodeList = document.querySelectorAll('.num');
+    numbersNodeList.forEach((button)=>{
+        button.addEventListener('click', ()=>{
+            updateDisplayNumber(button.textContent);
+        })
+    })
+}
+
+function emptyDisplay(){
+    const displayBoard = document.querySelector('.display');
+    return displayBoard.textContent === '';
+}
+
+function getDisplayNumber(){
+    const displayBoard = document.querySelector('.display');
+    return displayBoard.textContent;
+}
+
+function clearDisplay(){
+    const displayBoard = document.querySelector('.display');
+    displayBoard.textContent = '';
+}
+
+let memory = {
+    firstNum: '',
+    secondNum: '',
+    operation: '',
+    gotOp: false
+};
+
+
+
+function resetMemory(){
+    memory.firstNum = '';
+    memory.secondNum = '';
+    operation = '';
+}
+function handleOperationButtons(){
+    const operationsNodeList = document.querySelectorAll('.op');
+    operationsNodeList.forEach((button)=>{
+        button.addEventListener('click', ()=>{
+            
+            
+        })
+    })
+}
+
+
+
+handleNumberButtons();
+handleOperationButtons();
